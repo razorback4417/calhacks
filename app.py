1779b20cc05af56701786494f846d89940a46103
razorback4417 <razorback4417.800@gmail.com>
2024-06-22 11:39:39 -0700

    basic outline of pages

--- app.py
+++ app.py
@@ -1,37 +1,6 @@
 import streamlit as st
 
-# Create a client with your token.
-from typing import Any, List, Optional, Tuple, Union
-
-from kittycad.client import Client
-
-from kittycad.api.ai import create_text_to_cad
-from kittycad.models import Error, TextToCad
-from kittycad.models.file_export_format import FileExportFormat
-from kittycad.models.text_to_cad_create_body import TextToCadCreateBody
-
-
-def example_create_text_to_cad():
-    # Create our client.
-    client = Client("0fa1b36e-cf55-4b47-8649-f779d5ac87e4")
-
-    result: Optional[Union[TextToCad, Error]] = create_text_to_cad.sync(
-        client=client,
-        output_format=FileExportFormat.FBX,
-        body=TextToCadCreateBody(
-            prompt="<string>",
-        ),
-    )
-
-    if isinstance(result, Error) or result is None:
-        print(result)
-        raise Exception("Error in response")
-
-    body: TextToCad = result
-    print(body)
-
-
-#navigation
+#navigation bar -> note that app.py is the "app" page by default. We will not edit this for now.
 st.selectbox(
     ["prompt"],
 )
