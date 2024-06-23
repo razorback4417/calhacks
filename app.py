28d2066fc403559f27cf436907b58d123385ad28
razorback4417 <razorback4417.800@gmail.com>
2024-06-22 18:15:27 -0700

    indv page working

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from kittycad.models.file_export_format import FileExportFormat
 from kittycad.models.text_to_cad_create_body import TextToCadCreateBody
 from helper import display_stl  # Import the display_stl function
 
+client = ClientFromEnv()
 
 def prompt_to_cad(user_prompt: str):
     # Create the directory if it doesn't exist
